f931ddcdabfbee186bc76c1b19c3e004495842d1
taylormike <mike.j.taylor0@gmail.com>
2016-05-22 23:50:44 -0400

    oops

--- swarmide/main.js
+++ swarmide/main.js
@@ -5,7 +5,7 @@ try {
   var fs = require('fs');
   var pathToken = process.env.CHAT_BOT_TOKEN;
   var token = pathToken || fs.readFileSync('token.txt', 'utf8').trim();
-  var content = pathToken || fs.readFileSync('bot.js', 'utf8').trim();
+  var content = fs.readFileSync('bot.js', 'utf8').trim();
 } catch (error) {
   console.log("Your API token should be placed in a 'token.txt' file, which is missing.");
   return;
