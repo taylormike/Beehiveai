cbfad7caadfecb2b1a2f56354c912169bedd822a
taylormike <mike.j.taylor0@gmail.com>
2016-05-28 15:46:02 -0400

    upgrading to new version of slack-client

--- swarmide/bot.js
+++ swarmide/bot.js
@@ -1,6 +1,8 @@
+const RtmClient = require('@slack/client').RtmClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
 const rx = require('rx');
 const _ = require('underscore-plus');
-const Slack = require('./slack-client');
 
 const MessageHelpers = require('./message-helpers');
 
@@ -10,53 +12,26 @@ class Bot {
   //
   // token - An API token from the bot integration
   constructor(token) { 
-    this.slack = new Slack(token, true, true);
+    this.rtm = new RtmClient(token, {logLevel: 'debug'});
   }
 
   // Public: Brings this bot online and starts handling messages sent to it.
   login() {
-    rx.Observable.fromEvent(this.slack, 'open')
-      .subscribe(() => this.onClientOpened());
-
-    this.slack.login();
+   this.rtm.start();
+   this.respondToSnippet();
   }
 
-  // Public: Edits a snippet posted in the chat.  
-  editSnippet(content) {
-    this.slack.filesEdit(content);
-  } 
 
+  respondToSnippet() {
+    this.rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+     console.log('Message!!!!!!!!!!!!!!!!!!!!!');
+   });
 
-  // Private: Save which channels and groups this bot is in and log them.
-  onClientOpened() {
-    this.channels = _.keys(this.slack.channels)
-      .map(k => this.slack.channels[k])
-      .filter(c => c.is_member);
+    this.rtm.on(RTM_EVENTS.FILE_SHARED, function (message) {
+     console.log('File!!!!!!!!!!!!!!!!!!!!!!!!');
+   });
+  }   
 
-    this.groups = _.keys(this.slack.groups)
-      .map(k => this.slack.groups[k])
-      .filter(g => g.is_open && !g.is_archived);
-
-    this.dms = _.keys(this.slack.dms)
-      .map(k => this.slack.dms[k])
-      .filter(dm => dm.is_open);
-
-    console.log(`Welcome to Slack. You are ${this.slack.self.name} of ${this.slack.team.name}`);
-
-    if (this.channels.length > 0) {
-      console.log(`You are in: ${this.channels.map(c => c.name).join(', ')}`);
-    } else {
-      console.log('You are not in any channels.');
-    }
-
-    if (this.groups.length > 0) {
-      console.log(`As well as: ${this.groups.map(g => g.name).join(', ')}`);
-    }
-
-    if (this.dms.length > 0) {
-      console.log(`Your open DM's: ${this.dms.map(dm => dm.name).join(', ')}`);
-    }
-  }
 }
 
 module.exports = Bot;
