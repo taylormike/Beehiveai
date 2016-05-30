47700a747fa13d670b332170aedaccc0d6366342
taylormike <mike.j.taylor0@gmail.com>
2016-05-30 18:56:51 -0400

    Swarmide Web UI initial checkin (Jade Version)

--- swarmide/bot.js
+++ swarmide/bot.js
@@ -1,33 +1,65 @@
+var express = require('express');
+var http = require('http');
 const RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var WebClient = require('@slack/client').WebClient;
 
 const MessageHelpers = require('./message-helpers');
+var rtm;
+var web;
+var app;
+var pub; 
 
 class Bot {
   // Public: Creates a new instance of the bot.
   //
   // token - An API token from the bot integration
   constructor(token) { 
-    this.rtm = new RtmClient(token);
+    rtm = new RtmClient(token);
+    web = new WebClient(token);
+    app = express();
+    pub = __dirname + '/public';
   }
 
   // Public: Brings this bot online and starts handling messages sent to it.
   login() {
-   this.rtm.start();
+   rtm.start();
+   this.setupUI();
    this.respondToSnippet();
   }
 
-
+  setupUI() {
+   //this.app = express();
+   //this.pub = __dirname + '/public';
+   app.use(express.static(pub));
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'jade');
+   app.use(function(err, req, res, next) {
+     res.send(err.stack);
+   });
+   app.listen(3000);
+   console.log('Express started on port 3000');
+  }
+  
   respondToSnippet() {
-    this.rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-     console.log('Message!!!!!!!!!!!!!!!!!!!!!');
-   });
+    rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+      console.log('Message!!!!!!!!!!!!!!!!!!!!!!!!');
+      var user = rtm.dataStore.getUserById(message.user)
+      var dm = rtm.dataStore.getDMByName(user.name);
+      rtm.sendMessage('Code Shared: http://locahost:3000', dm.id);
+    }); 
 
-    this.rtm.on(RTM_EVENTS.FILE_SHARED, function (message) {
+    rtm.on(RTM_EVENTS.FILE_SHARED, function (message) {
      console.log('File!!!!!!!!!!!!!!!!!!!!!!!!');
+     console.log(message);
+     web.files.info(message.file_id, null, function handleContentFileUpload(err, res) {
+      var responseContent = res.content;
+      app.get('/', function(req, res){
+       res.render('users', {text: responseContent});
+      });
+     }); 
    });
-  }   
-
+  }
 }
-
+   
 module.exports = Bot;
